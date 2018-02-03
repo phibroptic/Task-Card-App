128c3666063a13b9eea6c2ef274f5608cbbaf564
LAPTOP-CEIOIKUG\mikee <mike.eric@gmail.com>
2018-02-03 12:27:52 -0500

    I reorganized the folder strcture and updated the ignore file to ignore the node_modules

--- skeleton/app/card/card.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    moduleId: module.id,
-    selector: 'app-card',
-    templateUrl: 'card.component.html',
-    styleUrls: [ 'card.component.css' ]
-})
-export class CardComponent { }
